7989ae707b0bda76662d6f56e441f9a59c7efbb9
Jonathan Hood <jonathan.hood@adtran.com>
2015-03-30 12:07:34 -0500

    Add better example for starting/stopping in bash

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,37 @@
-#!/bin/bash`
+#!/bin/bash
 
-( 
-    setsid echo "Hello World" > "out.log" 2>&1 < /dev/null & 
-    echo $! > "out.pid"
-)
+DAEMON_NAME="test-daemon.sh"
+DAEMON_RUN="./$DAEMON_NAME"
+PIDFILE="out.pid"
+PID=""
+
+
+if [[ -e $PIDFILE ]]; then
+    TEMP_PID=$(cat $PIDFILE)
+    MATCHING_PROCESSES=$(pgrep $DAEMON_NAME)
+    if [[ $MATCHING_PROCESSES =~ $TEMP_PID ]]; then
+        PID=$TEMP_PID
+    fi
+fi
+
+case $1 in
+    "start")
+        if [[ -z $PID ]]; then
+            setsid $DAEMON_RUN > "out.log" 2>&1 < /dev/null & 
+            echo $! > "out.pid"
+            echo "Started"
+        else
+            echo "Already Running"
+        fi
+        ;;
+    "stop")
+        if [[ -n $PID ]]; then
+            kill $PID
+            echo "Stopped"
+        else
+            echo "Not Running"
+        fi
+        ;;
+esac
+            
 
